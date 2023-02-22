de268f1a9a1fdccab81cbd46eb1dd80e592704ef
tastasterone <tastankulmeshkenov@gmail.com>
2023-02-21 21:37:55 -0600

    add logo, update forms

--- static/app.js
+++ static/app.js
@@ -2,12 +2,14 @@ const client = new Twilio.Conversations.Client(token);
 const signupForm = document.querySelector('#signup_form')
 const loginForm = document.querySelector('#login_form')
 const loginButtonSwitch = document.querySelector('.login_button_switch')
+const smallFormContainer = document.querySelector('.small_forms_container')
 
 
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarNav = document.querySelector('.navbar-nav');
 
-loginForm.style.visibility = 'hidden'
+loginForm.style.display='none';
+smallFormContainer.style.display="none";
 
 navbarToggle.addEventListener('click', () => {
   navbarNav.classList.toggle('active');
